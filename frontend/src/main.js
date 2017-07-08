fb663ef34ae7566368c8705837a8ec09075cacde
Ni_Chujie <ncj19991213@126.com>
2017-07-08 20:38:08 +0800

    update

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,10 +4,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
-import fullCalendar from 'vue-fullcalendar'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
-Vue.component('full-calendar', fullCalendar)
+
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 

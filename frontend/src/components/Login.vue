51c6f812060d641b0be3da4bcd3ab785b3126dc4
Ni_Chujie <ncj19991213@126.com>
2017-07-08 20:57:13 +0800

    update

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -3,35 +3,33 @@
     <el-form-item label="用户名">
       <el-input v-model="LoginForm.Username"></el-input>
     </el-form-item>
-    <el－form－item label="密码">
+    <el-form-item label="密码">
       <el-input type="password" v-model="LoginForm.Password"></el-input>
     </el-form-item>
     <el-form-item>
-      <el-button type="small" @click="LoginSubmit()">登陆</el-button>
+      <el-button type="small" @click="LoginSubmit">登陆</el-button>
     </el-form-item>
   </el-form>
 </template>
 <script>
-</script>
-<style>
   import axios from 'axios'
   export default {
     data () {
       return {
-        LoginForm :{
-          "Username":'',
-          "Password": ''
+        LoginForm: {
+          'Username': '',
+          'Password': ''
         }
       }
     },
     methods: {
       LoginSubmit () {
         axios({
-          methods: "POST",
+          methods: 'POST',
           url: '',
           data: JSON.stringify({
-            "Username": this.LoginForm.Username,
-            "Password": this.LoginForm.Password
+            'Username': this.LoginForm.Username,
+            'Password': this.LoginForm.Password
           })
         }.bind(this)).then(function (response) {
           if (response.data.message === 'Success') {
@@ -45,14 +43,19 @@
               title: '错误',
               message: '登陆失败'
             })
-          } else ｛
-          this.$notify.error({
-            title: '错误',
-            message: '服务器出错'
-          })
-          ｝
+          } else {
+            this.$notify.error({
+              title: '错误',
+              message: '服务器出错'
+            })
+          }
         }.bind(this))
+          .catch(function (error) {
+            console.log(error)
+          })
       }
     }
   }
+</script>
+<style>
 </style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Calendar from '@/pages/Calendar'
+import Login from '@/components/Login'
 
 Vue.use(Router)
 
@@ -15,6 +16,11 @@ export default new Router({
       path: '/day',
       name: 'Day',
       component: Calendar
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
     }
   ]
 })

3ff570a16ca6d2debdd6294764193bd70545840c
wto <qiushimao@hotmail.com>
2017-07-08 17:45:02 +0800

    update

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
-
+import Test from '@/components/EventForm'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'Hello',
-      component: Hello
+      path: '/test',
+      name: 'test',
+      component: Test
     }
   ]
 })

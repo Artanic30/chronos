3f446b5866eb6aa89089937545ec3b116f965c26
wto <qiushimao@hotmail.com>
2017-07-09 07:02:16 +0800

    update

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="container">
     <ChronosNav></ChronosNav>
     <router-view></router-view>
   </div>
--- frontend/src/components/Calendar.vue
+++ frontend/src/components/Calendar.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="container">
-    <el-row class="main">
-      <el-col :span="3" v-for="index in week" class="card">{{ index }}</el-col>
-    </el-row>
-    <el-row class="main" v-for="weekIndex in month">
-      <el-col :span="3" v-for="dayIndex in weekIndex" class="card">{{ dayIndex }}</el-col>
+    <el-row :gutter="20" class="tac">
+      <el-col :span="leftWidth" :offset="2">
+        <el-row class="main">
+          <el-col :span="3" v-for="index in week" class="card" :key="index">{{ index }}</el-col>
+        </el-row>
+        <el-row class="main" v-for="weekIndex in month" :key="weekIndex">
+          <el-col :span="3" v-for="dayIndex in weekIndex" :key="dayIndex">
+            <div class="card" @click="editOrBrowse">{{ dayIndex }}</div>
+          </el-col>
+        </el-row>
+      </el-col>
+      <el-col :span="rightWidth" v-if="state === 'edit' ">
+        <event-form></event-form>
+      </el-col>
     </el-row>
   </div>
 </template>
 
 <script>
+  import EventForm from '@/components/EventForm'
   export default {
+    components: {
+      'EventForm': EventForm
+    },
     data () {
       return {
+        leftWidth: 24,
+        rightWidth: 10,
+        state: 'browse',
         week: [
           '星期日',
           '星期一',
@@ -38,13 +54,24 @@
       }
     },
     methods: {
+      editOrBrowse () {
+        if (this.$data.state === 'edit') {
+          this.$data.state = 'browse'
+          this.$data.leftWidth = 24
+          this.$data.rightWidth = 0
+        } else if (this.$data.state === 'browse') {
+          this.$data.state = 'edit'
+          this.$data.leftWidth = 14
+          this.$data.rightWidth = 10
+        }
+      }
     }
   }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .card{
+  .card {
     background-color: #72f1ff;
     border: 1px solid #fff;
     height: 150px;
